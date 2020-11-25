d21320a87f46ff2fd4f326112ec54130b90a0112
BlackChep1 <kolynazaza@gmail.com>
2020-11-25 16:57:21 +0300

    1 commit

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        private string value1;
+        public Class1(string value1)
+        {
+            this.value1 = value1;
+        }
+        public string getCos()
+        {
+          //  double angle = Math.PI * degrees / 180.0;
+            return ""+ Math.Cos(Double.Parse(value1));
+        }
+        public string Clear()
+        {
+            return "";
+        }
+        public bool isEmpty()
+        {
+            bool flag = true;
+            if (value1.Length == 0 || Double.Parse(value1) < 0 || Double.Parse(value1) > 360)
+                flag = false; 
+            return flag;
+        }
+    }
+}
---
+++ LR1(v2)/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ClassLibrary1;
+
+namespace LR1_v2_
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click1(object sender, RoutedEventArgs e)
+        {
+            Class1 JohnCos = new Class1(tBox_Cos.Text);
+            if (JohnCos.isEmpty())
+                lb_answer.Content = JohnCos.getCos();
+        }
+
+        private void Button_Click2(object sender, RoutedEventArgs e)
+        {
+            tBox_Cos.Clear();
+            Class1 Clear = new Class1(tBox_Cos.Text);
+            lb_answer.Content = Clear.Clear();
+
+        }
+       
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Cos_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (Regex.IsMatch(tBox_Cos.Text, "[^0-9-,]")) 
+            {
+                tBox_Cos.Text = tBox_Cos.Text.Remove(tBox_Cos.Text.Length - 1);
+                tBox_Cos.SelectionStart = tBox_Cos.Text.Length;
+            }
+        }
+    }
+}
